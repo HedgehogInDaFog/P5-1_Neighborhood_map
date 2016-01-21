748f259daffcf6fe9919e59410cd8c6b63c732d4
vvorotnikov <v.s.vorotnikov@gmail.com>
2016-01-21 18:05:14 +0300

    First try on markers.  MVVM isn't done yet

--- js/model.js
+++ js/model.js
@@ -1,7 +1,23 @@
 var mapData = {
-    lat : 55.836575,
-    lng : 37.609103,
-    zoom : 8
+	mapCenter : {
+    	position : {
+    		lat : 56.236575,
+    		lng : 37.809103
+    	},
+    	zoom : 8
+    },
+
+    markers : [
+    	{
+			position : {lat: 56.236575, lng : 37.809103},
+    		title : "Marker #1"
+    	},
+    	{
+			position : { lat: 57.236575, lng : 38.809103},
+    		title : "Marker #2"
+    	},
+
+    ]
 };
 
 
--- js/view.js
+++ js/view.js
@@ -1,7 +1,18 @@
 var map;
+var markers = [];
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: mapData.lat, lng: mapData.lng},
-    zoom: mapData.zoom
+    center: mapData.mapCenter.position,
+    zoom: mapData.mapCenter.zoom
   });
+
+	len = mapData.markers.length;
+	for (var i = 0; i < len; i++) {
+	  markers.push(new google.maps.Marker({
+	    position: mapData.markers[i].position,
+	    map: map,
+	    title: mapData.markers[i].title
+	  }));
+	}
 }
