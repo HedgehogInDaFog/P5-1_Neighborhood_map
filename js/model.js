ccbba749ee7c044ff0a0861936c8b2a88af879c0
vvorotnikov <v.s.vorotnikov@gmail.com>
2016-02-02 17:55:48 +0300

    Add some cities to model

--- js/model.js
+++ js/model.js
@@ -4,30 +4,44 @@ var generalMapData = {
     		lat : 56.236575,
     		lng : 37.909103
     	},
-    	zoom : 8
+    	zoom : 7
     },
 
     markers : [
     	{
-			position : {lat: 56.307384, lng : 38.15088},
+			position : {lat: 56.307, lng : 38.150},
     		title : "Sergiyev Posad"
     	},
     	{
-			position : { lat: 56.165470, lng : 40.402882},
+			position : { lat: 56.165, lng : 40.402},
     		title : "Vladimir"
     	},
         {
-            position : { lat: 56.416168, lng : 40.453777},
+            position : { lat: 56.416, lng : 40.453},
             title : "Suzdal"
         },
         {
-            position : { lat: 55.603643, lng : 40.653872},
-            title : "Gus-Khrustalny"
+            position : { lat: 56.736, lng : 38.882},
+            title : "Pereslavl-Zalessky"
         },
         {
-            position : { lat: 56.736716, lng : 38.882745},
-            title : "Pereslavl-Zalessky "
+            position : { lat: 57.633, lng : 39.855},
+            title : "Yaroslavl"
+        },
+        {
+            position : { lat: 57.186, lng : 39.411},
+            title : "Rostov"
+        },
+        {
+            position : { lat: 57.765, lng : 40.925},
+            title : "Kostroma"
         }
+        /*
+        {
+            position : { lat: 55.995, lng : 37.212},
+            title : "Zelenograd"
+        }
+        */
 
     ]
 };
