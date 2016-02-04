68148decaa97188824f6e894a61ab708ba164813
vvorotnikov <v.s.vorotnikov@gmail.com>
2016-02-04 16:21:13 +0300

    Start using Gulp
    type: chore
    
    After modification by Gulp files are stored at ./dst/ folder

--- dst/js/app.js
+++ dst/js/app.js
@@ -287,3 +287,46 @@ var googleSuccess = function() {
 
     ko.applyBindings(new ViewModel());
 };
+var mapData = {
+
+	mapCenter : {
+    	position : {
+    		lat : 56.236575,
+    		lng : 37.909103
+    	},
+    	zoom : 7
+    },
+
+    markers : [
+    	{
+			position : {lat: 56.307, lng : 38.150},
+    		title : "Sergiyev Posad"
+    	},
+    	{
+			position : { lat: 56.165, lng : 40.402},
+    		title : "Vladimir"
+    	},
+        {
+            position : { lat: 56.416, lng : 40.453},
+            title : "Suzdal"
+        },
+        {
+            position : { lat: 56.736, lng : 38.882},
+            title : "Pereslavl-Zalessky"
+        },
+        {
+            position : { lat: 57.633, lng : 39.855},
+            title : "Yaroslavl"
+        },
+        {
+            position : { lat: 57.186, lng : 39.411},
+            title : "Rostov"
+        },
+        {
+            position : { lat: 57.765, lng : 40.925},
+            title : "Kostroma"
+        }
+    ]
+};
+
+
