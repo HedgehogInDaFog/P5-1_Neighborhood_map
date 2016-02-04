e0a86d75a392536c7c1a1ed94ac1cbecae655321
vvorotnikov <v.s.vorotnikov@gmail.com>
2016-02-04 13:17:43 +0300

    type: style
    - add comments
    - some fixes after jslint

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ var googleSuccess = function() {
     var ViewModel = function() {
 
         var self = this;
-        this.map;                                               //Google map
+        this.map = {};                                          //Google map
         this.markerList = ko.observableArray([]);               //The list of markers
         this.currentMarkerList = ko.observableArray([]);        //The list of currently visible markers
         this.searchString = ko.observable('');                  //Current text in search string
@@ -28,7 +28,7 @@ var googleSuccess = function() {
 
         this.infoWindowsOpened = [];
 
-        this.contentStringTemplate = '<div class="container"><div class="full-width"><h3>%Label%</h3></div>' +
+        this.contentStringTemplate = '<div class="container"><div class="full-width"><h4>%Label%</h4></div>' +
         '<div class="full-width"><a href="%WikiLinkLoc%">%WikiLinkText%</a><p>%WikiInfo%</p></div>' +
         '<div class="full-width"><img class="image-flickr" src=%Image0% alt="city image"></img>' +
         '<img class="image-flickr" src=%Image1% alt="city image"></img>' +
@@ -38,7 +38,7 @@ var googleSuccess = function() {
         * @function
         * @description Initialize map and all markers
         * @param {object} mapCenter - JSON, containing data about initial map center (see model.js for more details)
-        * @param {object} markers - JSON, containing data about markers
+        * @param {object} markers - JSON, containing data about markers (see model.js for more details)
         */
         this.initMap = function(mapCenter, markers) {
             //create map
@@ -61,6 +61,11 @@ var googleSuccess = function() {
 
         /*----------=========Functions, working with 3rd-party APIs=========---------*/
 
+        /**
+        * @function
+        * @description Get data from Wikipedia API
+        * @param {object} markers - list, containing Google Map Markers
+        */
         this.getInfoFromWiki = function(markers) {
 
             var urlWikiRequest = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%request%&format=json&callback=wikiCallback';
@@ -83,13 +88,18 @@ var googleSuccess = function() {
             });
         };
 
+        /**
+        * @function
+        * @description Get data from Flickr API
+        * @param {object} markers - list, containing Google Map Markers
+        */
         this.getInfoFromFlickr = function(markers) {
 
             var urlFlickrRequest = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ec08162143dcb46549ffc0535cdbc2cc&' +
             '&tags=%tags%&sort=interestingness-desc&per_page=3&content_type=1&' +
             'media=photos&nojsoncallback=1&format=json&lat=%lat%&lon=%lon%';
 
-            var urlFlirckImageTemplate = 'https://farm%farm-id%.staticflickr.com/%server-id%/%id%_%secret%_q.jpg';
+            var urlFlirckImageTemplate = 'https://farm%farm-id%.staticflickr.com/%server-id%/%id%_%secret%_q.jpg'; //helps to get URL of the photo, from Flickr data
 
             var tagList = 'church, monument, square, nature, travel, architecture, city, museum, flowers, holidays';
 
@@ -134,40 +144,45 @@ var googleSuccess = function() {
         this.hideButtonClick = function() {
 
             var absCont = $('#main-container');
+            var deltaY = 0;
 
             if (self.hideButton() == '▲') {
                 self.hideButton('▼');
-                var deltaY = absCont.offset().top - absCont.height() + 20;
+                deltaY = absCont.offset().top - absCont.height() + 20;
                 absCont.offset({ top: deltaY, left: 0 });
             } else {
                 self.hideButton('▲');
-                var deltaY = absCont.offset().top + absCont.height() - 20;
+                deltaY = absCont.offset().top + absCont.height() - 20;
                 absCont.offset({ top: deltaY, left: 0 });
             }
         };
 
         this.markerClick = function (marker) {
 
+            // close all opened infowindows
             self.infoWindowsOpened.forEach(function(infowindow) {
                 infowindow.close();
             });
 
+            // animate marker for 2 seconds after click
             marker.setAnimation(google.maps.Animation.BOUNCE);
             setTimeout(function() {
                 marker.setAnimation(null);
             }, 2000);
 
+            //create new infowindow
             var infowindow = new google.maps.InfoWindow({
                 content: self.markerDataToHTML(marker, self.contentStringTemplate)
             });
 
+            //open new infowindow and add it to a list of opened
             infowindow.open(self.map, marker);
             self.infoWindowsOpened.push(infowindow);
         };
 
         this.showFavouritesClick = function() {
 
-            if (self.showOnlyFavourites() == false) {
+            if (self.showOnlyFavourites() === false) {
                 self.showOnlyFavourites(true);
             } else {
                 self.showOnlyFavourites(false);
@@ -177,7 +192,7 @@ var googleSuccess = function() {
 
         this.starButtonClick = function() {
 
-            if (this.isFavourite() == true) {
+            if (this.isFavourite() === true) {
                 this.isFavourite(false);
             } else {
                 this.isFavourite(true);
@@ -198,20 +213,30 @@ var googleSuccess = function() {
           return marker;
         };
 
+        /**
+        * @function
+        * @description Turn 3rd-party data (Wiki, Flickr) into HTML for InfoWindow
+        * @param {object} marker - Google Map Marker for which we create HTML
+        * @param {string} template - HTML Template, in which we add data from 3rd-party APIs
+        */
         this.markerDataToHTML = function(marker, template) {
 
+            //Add Wikipedia data
             var result = template.replace('%Label%',marker.title);
-            if (marker.wikiSuccess == true) {
+            if (marker.wikiSuccess === true) {
                 result = result.replace('%WikiInfo%', marker.wikiData[2][0]);
                 result = result.replace('%WikiLinkText%', marker.wikiData[1][0]).replace('%WikiLinkLoc%', marker.wikiData[3][0]);
             } else {
+            //In case, wikipedia request was unsuccessful we still need to write something for user
                 result = result.replace('%WikiInfo%', 'Sorry, we can\'t get information from Wikipedia :-(');
                 result = result.replace('%WikiLinkText%', '').replace('%WikiLinkLoc%', '');
             }
 
-            if (marker.flickrSuccess == true) {
+            //Add Flickr data
+            if (marker.flickrSuccess === true) {
                 result = result.replace('%Image0%',marker.flickrData[0]).replace('%Image1%',marker.flickrData[1]).replace('%Image2%',marker.flickrData[2]);
             } else {
+            //In case, flickr request was unsuccessful we still need to show something for user
                 result = result.replace('%Image0%','images/err.png').replace('%Image1%','images/err.png').replace('%Image2%','images/err.png');
                 result = result.replace('city image', 'Images from Flickr cannot be downloaded');
             }
@@ -221,20 +246,31 @@ var googleSuccess = function() {
 
         /*----------=========Search functions=========---------*/
 
+        /**
+        * @function
+        * @description Filter list of cities and markers
+        */
         this.filterLocations = function() {
 
+            /**
+            * @function
+            * @description Set map parametr for list of markers
+            * @param {object} map - Google Map Object (or "Null" if we want to hide markers)
+            * @param {object} markers - List of Google Map Marker for which we set "map"
+            */
             function setMapOnAll(map, markers) {
                 for (var i = 0; i < markers.length; i++) {
                     markers[i].setMap(map);
                 }
-            };
+            }
+
             //filter list of items
             var str = this.searchString().toLowerCase();
             var tmpArray = [];
             var len = this.markerList().length;
             for (var i = 0; i < len; i++) {
                 if (self.markerList()[i].title.toLowerCase().indexOf(str) != -1) {
-                    if ((self.markerList()[i].isFavourite() == true) || (self.showOnlyFavourites() == false)) {
+                    if ((self.markerList()[i].isFavourite() === true) || (self.showOnlyFavourites() === false)) {
                         tmpArray.push(self.markerList()[i]);
                     }
                 }
@@ -245,17 +281,8 @@ var googleSuccess = function() {
             setMapOnAll(null, this.markerList());
             setMapOnAll(this.map, this.currentMarkerList());
         };
-
         this.initMap(mapData.mapCenter, mapData.markers);
-
     };
 
     ko.applyBindings(new ViewModel());
-
 };
-
-
-
-
-
-
